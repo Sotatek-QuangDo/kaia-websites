ece0aa7d302b01b56dbb082e15c44c32c1767410
Sotatek-QuangDo <quang.do@sotatek.com>
2025-09-23 09:03:19 +0700

    fix: update faucet

--- js/main/pages/faucet/index.js
+++ js/main/pages/faucet/index.js
@@ -1,5 +1,86 @@
 const APIEntry = "https://api-homepage.kaia.io";
 
+const loadState = (_data) => {
+  if (_data.hasOwnProperty("balance")) {
+    document.getElementById("kaia_balance").innerHTML = _data.balance || "0";
+  }
+
+  if (_data.hasOwnProperty("isInvalidAddress")) {
+    if (_data.isInvalidAddress) {
+      document.getElementById("kaia_address_error_msg").style.visibility =
+        "visible";
+      document.getElementById("kaia_address").style.color = "#E85B56";
+      document.getElementById("kaia_address_label").style.color = "#E85B56";
+      document.getElementById("kaia_address").style.border =
+        "1px solid #E85B56";
+    } else {
+      document.getElementById("kaia_address_error_msg").style.visibility =
+        "hidden";
+      document.getElementById("kaia_address").style.color =
+        "rgba(255, 255, 255, .4)";
+      document.getElementById("kaia_address_label").style.color =
+        "rgba(255, 255, 255, .99)";
+      document.getElementById("kaia_address").style.border =
+        "1px solid #000000";
+    }
+  }
+
+  if (_data.hasOwnProperty("popupShow")) {
+    if (_data.popupShow) {
+      if (_data.isError) {
+        document.getElementById("kaia_modal").style.backgroundColor = "#5d2422";
+        document.getElementById("kaia_modal").style.border =
+          "1px solid rgba(232, 91, 86, 0.15)";
+        document.getElementById("kaia_modal").style.boxShadow =
+          "0px 80x 16px 0px rgba(235, 128, 122, 0.25)";
+        document.getElementById("kaia_modal_title").style.color = "#f0a6a0";
+        document.getElementById("kaia_modal_content").style.color = "#f0a6a0";
+        document.getElementById("kaia_modal_success_warning").style.visibility =
+          "hidden";
+        document.getElementById("kaia_modal_success_close").style.visibility =
+          "hidden";
+        document.getElementById("kaia_modal_fail_warning").style.visibility =
+          "visible";
+        document.getElementById("kaia_modal_fail_close").style.visibility =
+          "visible";
+      } else {
+        document.getElementById("kaia_modal").style.backgroundColor = "#1f5214";
+        document.getElementById("kaia_modal").style.border =
+          "1px solid rgba(64, 171, 43, 0.25)";
+        document.getElementById("kaia_modal").style.boxShadow =
+          "0px 80x 16px 0px rgba(87, 207, 63, 0.25)";
+        document.getElementById("kaia_modal_title").style.color = "#7eda6c";
+        document.getElementById("kaia_modal_content").style.color = "#7eda6c";
+        document.getElementById("kaia_modal_success_warning").style.visibility =
+          "visible";
+        document.getElementById("kaia_modal_success_close").style.visibility =
+          "visible";
+        document.getElementById("kaia_modal_fail_warning").style.visibility =
+          "hidden";
+        document.getElementById("kaia_modal_fail_close").style.visibility =
+          "hidden";
+      }
+      document.getElementById("kaia_modal").style.visibility = "visible";
+      document.getElementById("kaia_modal_bg").style.visibility = "visible";
+      document.getElementById("kaia_modal_title").innerHTML = _data.title;
+      document.getElementById("kaia_modal_content").innerHTML = _data.content;
+    } else {
+      document.getElementById("kaia_modal_success_warning").style.visibility =
+        "hidden";
+      document.getElementById("kaia_modal_success_close").style.visibility =
+        "hidden";
+      document.getElementById("kaia_modal_fail_warning").style.visibility =
+        "hidden";
+      document.getElementById("kaia_modal_fail_close").style.visibility =
+        "hidden";
+      document.getElementById("kaia_modal_title").innerHTML = _data.title;
+      document.getElementById("kaia_modal_content").innerHTML = _data.content;
+      document.getElementById("kaia_modal").style.visibility = "hidden";
+      document.getElementById("kaia_modal_bg").style.visibility = "hidden";
+    }
+  }
+};
+
 function isAddress(address) {
   if (typeof address !== "string") return false;
   if (address.length !== 42) return false;
